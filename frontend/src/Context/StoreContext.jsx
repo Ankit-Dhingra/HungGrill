9499e275029270058a21eab45dbe17fc69af1c1d
Ankit Dhingra <165490728+Ankit-Dhingra@users.noreply.github.com>
2025-08-02 23:14:54 +0530

    Update Backend URL in Frontend/StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "http://localhost:4000"
+    const url = "https://hunggrill-backend.onrender.com"
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("");
